e5e5e158ce380cf640c0a2b35fe69a0cc01dab2d
Mohammed <devhammed@gmail.com>
2024-01-18 06:59:43 +0000

    finalized backend tests

--- config/database.php
+++ config/database.php
@@ -42,7 +42,13 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.test.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
---
+++ tests/Unit/CoffeeSalesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+
+class CoffeeSalesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * get sales route.
+     */
+    public function test_get_sales_route(): void
+    {
+        \Artisan::call('db:seed');
+        $user = User::where('id', 1)->first();
+        $response = $this->actingAs($user)
+                         ->get('/coffee-sales');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                ['sales', 'status']
+            );
+    }
+
+    /**
+     * post sale route.
+     */
+    public function test_post_sale_route(): void
+    {
+        \Artisan::call('db:seed');
+        $user = User::where('id', 1)->first();
+        $response = $this->actingAs($user)
+                         ->post('/coffee-sales/store', [
+                            'quantity' => '1',
+                            'unit_cost' => '10',
+                            'selling_price' => '23.33',
+                            'product' => 'gold'
+                         ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                ['sale', 'status']
+            );
+    }
+}
