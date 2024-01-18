bff7c153e829aa997316a9ba7381458179e6ced4
Mohammed <devhammed@gmail.com>
2024-01-18 07:25:51 +0000

    include coffee.js only where needed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 require('./bootstrap');
-require('./coffee.js')
 
 import Alpine from 'alpinejs';
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        {{ $scripts ? $scripts : null }}
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
